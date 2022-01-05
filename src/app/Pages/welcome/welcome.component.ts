2647d0d2476866bd6bdd679ceeab98f607b7ac7f
Teo230 <teeassassin230@gmail.com>
2022-01-05 02:51:29 +0100

    add some call

--- src/app/Pages/welcome/welcome.component.ts
+++ src/app/Pages/welcome/welcome.component.ts
@@ -6,15 +6,31 @@ import { WtmApiService } from 'src/app/services/wtm-api.service';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
+
 export class WelcomeComponent implements OnInit {
 
+  meme: any = {};
+
   constructor(private wtmService: WtmApiService) {
-    this.wtmService.GetRandomMemes(1).then((res)=>{
-      console.log(res);
-    });
+    this.getRandomMeme();
   }
 
   ngOnInit(): void {
   }
 
+  getRandomMeme(){
+    this.wtmService.GetRandomMemes(1).then((res:any)=>{
+      this.meme = res.memes[0];
+      //console.log(this.meme);
+    });
+  }
+
+  createRoom(){
+    this.wtmService.GenerateRoom().then((roomCode:any) => {
+      console.log(roomCode);
+      this.wtmService.JoinRoom(roomCode).then((room:any) => {
+        console.log(room);
+      });
+    });
+  }
 }
--- src/app/services/wtm-api.service.ts
+++ src/app/services/wtm-api.service.ts
@@ -2,9 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const imageService = "ImageManager/";
+const roomService = "Room/";
 
-const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
+const baseUrl = "http://157.230.24.19:8080/";
+const apiKey = "b851e72a-cad9-4eda-84a9-0d44eae13b80";
+const httpOptions: any = {
+  headers: new HttpHeaders({'apiKey' : apiKey,
+                            'Access-Control-Allow-Origin' : '*'}),
 }
 
 @Injectable({
@@ -13,18 +17,45 @@ const httpOptions = {
 
 export class WtmApiService {
 
-  baseUrl: string = "http://157.230.24.19:8080/";
-
   constructor(private httpClient: HttpClient){ 
   }
 
   public GetRandomMemes(count: number){
     return new Promise((resolve) => {
 
-      this.httpClient.get(this.baseUrl + imageService + "GetRandomMemes/" + count, httpOptions)
+      this.httpClient.get(baseUrl + imageService + "GetRandomMemes/" + count, httpOptions)
           .subscribe((res:any) =>{
             resolve(res);
-          }, error => console.log(error));
+          }, 
+          //error => console.log(error)
+      );
+    });
+  }
+
+  public GenerateRoom(){
+    return new Promise((resolve) => {
+      
+      let localOptions = httpOptions;
+      localOptions.responseType = "text";
+
+      this.httpClient.post(baseUrl + roomService + "GenerateRoom", null, localOptions)
+          .subscribe((res:any) =>{
+            resolve(res);
+          },
+          //error => console.log(error)
+      );
+    });
+  }
+
+  public JoinRoom(roomCode: string){
+    return new Promise((resolve) => {
+      let content = JSON.stringify({roomCode: roomCode});
+      this.httpClient.post(baseUrl + roomService + "JoinRoom", content, httpOptions)
+          .subscribe((res:any) =>{
+            resolve(res);
+          },
+          //error => console.log(error)
+      );
     });
   }
 }
